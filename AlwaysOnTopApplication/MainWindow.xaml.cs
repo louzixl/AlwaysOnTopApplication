6c40760950ccc4ec5fef74d6d641ced9c7e2c413
louzi_xl <louzi_xl@163.com>
2021-06-25 10:57:10 +0800

    Add files via upload

---
+++ AlwaysOnTopApplication/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace AlwaysOnTopApplication
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            // 使用方案一
+            HwndSource source = HwndSource.FromHwnd(new WindowInteropHelper(this).Handle);
+            if (source != null)
+                source.AddHook(WndProc);
+
+            // 使用方案二
+            // _ = SetTopMostLoop();
+
+            // 使用方案三
+            //_mouseHook = new MouseHook();
+            //_mouseHook.OnMouseActivity += MouseHook_OnMouseActivity;
+            //_mouseHook.Start();
+        }
+
+        /// <summary>
+        /// 方案一：捕获WM_WINDOWPOSCHANGING消息，若无SWP_NOZORDER标志，则置顶
+        /// </summary>
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case Win32Api.WM_WINDOWPOSCHANGING:
+                    Win32Api.WINDOWPOS wp = (Win32Api.WINDOWPOS)Marshal.PtrToStructure(
+                        lParam, typeof(Win32Api.WINDOWPOS));
+                    if ((wp.flags & Win32Api.SWP_NOZORDER) == 0)
+                        _ = SetTopMostLater();
+                    break;
+            }
+
+            return IntPtr.Zero;
+        }
+
+        private async Task SetTopMostLater()
+        {
+            await Task.Delay(300);
+            var interopHelper = new WindowInteropHelper(this);
+            Win32Api.SetWindowPos(interopHelper.Handle, Win32Api.HWND_TOPMOST, 0, 0, 0, 0, Win32Api.TOPMOST_FLAGS);
+        }
+
+        /// <summary>
+        /// 方案二：循环置顶
+        /// </summary>
+        /// <returns></returns>
+        private async Task SetTopMostLoop()
+        {
+            while (true)
+            {
+                await Task.Delay(2000);
+                var interopHelper = new WindowInteropHelper(this);
+                Win32Api.SetWindowPos(interopHelper.Handle, Win32Api.HWND_TOPMOST, 0, 0, 0, 0, Win32Api.TOPMOST_FLAGS);
+            }
+        }
+
+        // 方案三：当鼠标按下时置顶
+        private void MouseHook_OnMouseActivity(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            Console.WriteLine("mouse down......");
+            _ = SetTopMostLater();
+        }
+
+        private MouseHook _mouseHook;
+    }
+}
---
+++ AlwaysOnTopApplication/MouseHook.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace AlwaysOnTopApplication
+{
+    public class MouseHook
+    {
+        public MouseHook()
+        {
+        }
+
+        ~MouseHook()
+        {
+            Stop();
+        }
+
+        public void Start()
+        {
+            if (_hMouseHook == 0)
+            {
+                _mouseHookProcedure = new HookProc(MouseHookProc);
+                _hMouseHook = SetWindowsHookEx(WH_MOUSE_LL, _mouseHookProcedure, Marshal.GetHINSTANCE(System.Reflection.Assembly.GetExecutingAssembly().GetModules()[0]), 0);
+
+                if (_hMouseHook == 0)
+                {
+                    Stop();
+                    throw new Exception("SetWindowsHookEx failed.");
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            bool retMouse = true;
+
+            if (_hMouseHook != 0)
+            {
+                retMouse = UnhookWindowsHookEx(_hMouseHook);
+                _hMouseHook = 0;
+            }
+
+            if (!(retMouse))
+                throw new Exception("UnhookWindowsHookEx failed.");
+        }
+
+        private int MouseHookProc(int nCode, Int32 wParam, IntPtr lParam)
+        {
+            if ((nCode >= 0) && (OnMouseActivity != null))
+            {
+                MouseButtons button = MouseButtons.None;
+                int clickCount = 0;
+
+                switch (wParam)
+                {
+                    case WM_LBUTTONDOWN:
+                        button = MouseButtons.Left;
+                        clickCount = 1;
+                        break;
+                    case WM_RBUTTONDOWN:
+                        button = MouseButtons.Right;
+                        clickCount = 1;
+                        break;
+                    default:
+                        break;
+                }
+
+                MouseHookStruct MyMouseHookStruct = (MouseHookStruct)Marshal.PtrToStructure(lParam, typeof(MouseHookStruct));
+                MouseEventArgs e = new MouseEventArgs(
+                    button, clickCount, MyMouseHookStruct.pt.x, MyMouseHookStruct.pt.y, 0);
+
+                OnMouseActivity(this, e);
+            }
+
+            return CallNextHookEx(_hMouseHook, nCode, wParam, lParam);
+        }
+
+        public delegate int HookProc(int nCode, Int32 wParam, IntPtr lParam);
+        public event MouseEventHandler OnMouseActivity;
+
+        private HookProc _mouseHookProcedure;
+        private static int _hMouseHook = 0;
+
+        #region Win32Api
+        private const int WM_LBUTTONDOWN = 0x201;
+        private const int WM_RBUTTONDOWN = 0x204;
+        private const int WM_MBUTTONDOWN = 0x207;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public class POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public class MouseHookStruct
+        {
+            public POINT pt;
+            public int hWnd;
+            public int wHitTestCode;
+            public int dwExtraInfo;
+        }
+
+        public const int WH_MOUSE_LL = 14;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern int SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hInstance, int threadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern bool UnhookWindowsHookEx(int idHook);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern int CallNextHookEx(int idHook, int nCode, Int32 wParam, IntPtr lParam);
+        #endregion
+    }
+}
